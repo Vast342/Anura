d09bf1a0a7f8f6d6bd9ed47aae287d9356b559a5
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2024-12-11 10:00:48 -0500

    i32 to prevent overflows

--- src/nets/policy/mod.rs
+++ src/nets/policy/mod.rs
@@ -92,10 +92,10 @@ impl PolicyAccumulator {
     }
     pub fn get_score(&self, mov: Move, ctm: u8, king: Square) -> f32 {
         let move_index = move_index(ctm, mov, king);
-        let mut output = 0;
+        let mut output: i32 = 0;
         // hl -> output
         for hl_node in 0..HL_SIZE {
-            output += POLICY_NET.l2_weights[hl_node][move_index] * (activation(self.l1[hl_node]) / QA);
+            output += (POLICY_NET.l2_weights[hl_node][move_index] as i32) * (activation(self.l1[hl_node]) as i32);
         }
         (output as f32 / QA as f32 + POLICY_NET.l2_biases[move_index] as f32) / (QA as f32 * QB as f32)
     }
