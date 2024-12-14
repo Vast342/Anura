0d9cf7439f0d07f6de572111e3b5e4c078b2e87a
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2024-12-14 00:07:02 -0500

    better formatting, bench: 1130771

--- build.rs
+++ build.rs
@@ -11,18 +11,22 @@ fn main() {
         Ok(name) => name.trim().to_string(),
         Err(e) => panic!("Error reading policy.txt: {}", e),
     };
+    
     if value_network_name.is_empty() {
         panic!("value.txt contains no network name");
     }
     if policy_network_name.is_empty() {
         panic!("policy.txt contains no network name");
     }
+    
     let value_filename = format!("{}.vn", value_network_name);
     let policy_filename = format!("{}.pn", policy_network_name);
+    
     let value_path = PathBuf::from(format!("src/nets/value/{}", value_filename));
     let policy_path = PathBuf::from(format!("src/nets/policy/{}", policy_filename));
     download_network_if_not_exists(&value_network_name, &value_path, ".vn");
     download_network_if_not_exists(&policy_network_name, &policy_path, ".pn");
+
     println!("cargo:rerun-if-changed=value.txt");
     println!("cargo:rerun-if-changed=policy.txt");
     println!("cargo:rerun-if-changed={}", value_path.display());
@@ -31,14 +35,15 @@ fn main() {
 
 fn download_network_if_not_exists(network_name: &str, dest_path: &Path, file_extension: &str) {
     if dest_path.exists() {
-        println!("Network file {} already exists. Skipping download.", dest_path.display());
+        println!(
+            "Network file {} already exists. Skipping download.",
+            dest_path.display()
+        );
         return;
     }
     let url = format!(
-        "https://github.com/Vast342/anura-nets/releases/download/{}/{}{}", 
-        network_name, 
-        network_name, 
-        file_extension
+        "https://github.com/Vast342/anura-nets/releases/download/{}/{}{}",
+        network_name, network_name, file_extension
     );
     let output = Command::new("curl")
         .args(&["-sL", "-f", "-o", dest_path.to_str().unwrap(), &url])
