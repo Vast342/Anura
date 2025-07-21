b797dc773cbfede55ea869895384480f9b8828f6
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2025-07-21 17:43:46 -0400

    Update Copyright Year, bench: 686570

--- src/mcts/time.rs
+++ src/mcts/time.rs
@@ -2,7 +2,7 @@ use crate::tunable::Tunables;
 
 /*
     Anura
-    Copyright (C) 2024 Joseph Pasfield
+    Copyright (C) 2025 Joseph Pasfield
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
