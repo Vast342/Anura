b78653a92cc7320bdbe325ca1c4d14182c79841b
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2025-09-14 10:59:22 -0400

    avn_016_stupid, bench: 390529

--- src/nets/value.rs
+++ src/nets/value.rs
@@ -29,7 +29,7 @@ use crate::{
 
 const INPUT_SIZE: usize = 768 * 4;
 const HL_SIZE: usize = 1024;
-const OUTPUT_BUCKET_COUNT: usize = 16;
+const OUTPUT_BUCKET_COUNT: usize = 32;
 
 const COLOR_STRIDE: usize = 64 * 6;
 const PIECE_STRIDE: usize = 64;
